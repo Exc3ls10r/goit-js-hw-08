ec45682e3cc332fe0c07c273b9ce0dd3e8ab0eb4
Exc3ls10r <111652846+Exc3ls10r@users.noreply.github.com>
2023-03-26 23:02:01 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayer = new Player('vimeo-player');
+
+vimeoPlayer.on('timeupdate', throttle((e) => {
+localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000));
+
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  vimeoPlayer.setCurrentTime(currentTime);
+}
