3b9dc2a7d9753fef8bf84edb3df4567854164a12
Exc3ls10r <111652846+Exc3ls10r@users.noreply.github.com>
2023-03-26 22:53:51 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector('.gallery');
+
+function createGallery(items) {
+  return items.map( ({ preview, original, description }) => {
+    return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`
+  }).join("");
+};    
+
+galleryContainerRef.innerHTML = createGallery(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
